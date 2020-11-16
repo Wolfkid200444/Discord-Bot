ee2e56a50cfd840a47e9ca68d3de9c2e2c0c97a2
TheRealKizu <therealkizu@protonmail.com>
2020-11-16 14:13:24 +0800

    chore: added base command class

---
+++ src/structures/bot/BaseCommand.ts
@@ -0,0 +1,12 @@
+import { Command, CommandOptions } from "discord-akairo";
+import { BaseClient } from "./BaseClient";
+
+export class BaseCommand extends Command {
+
+    client: BaseClient;
+
+    constructor(id: string, options?: CommandOptions) {
+        super(id, options);
+    }
+
+}
