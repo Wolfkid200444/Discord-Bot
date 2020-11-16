c45962945c8b1647c6b102aebef303a1192ff6ba
TheRealKizu <therealkizu@protonmail.com>
2020-11-16 17:52:55 +0800

    chore: finished avatar command

--- src/commands/utility/AvatarCommand.ts
+++ src/commands/utility/AvatarCommand.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { GuildMember, Message, MessageEmbed, User } from "discord.js";
 import { BaseCommand } from "../../structures/bot/BaseCommand";
 
 class AvatarCommand extends BaseCommand {
@@ -8,13 +8,37 @@ class AvatarCommand extends BaseCommand {
             category: "utility",
             description: {
                 content: "Shows you an user's avatar",
+                usage: "<username | id | mention>"
             },
             aliases: ["pfp"],
+            channel: "guild",
+            args: [
+                {
+                    id: "user",
+                    type: "user"
+                }
+            ]
         });
     }
 
-    async exec(message: Message, args: any): Promise<any> {
-        //TODO: Finish this
+    async exec(message: Message, { user }: { user: GuildMember }): Promise<any> {
+        const embed = new MessageEmbed()
+            .setColor("0xBB94F8");
+
+        if (!user) {
+            embed.setDescription(`[**Image Link**](${user.user.displayAvatarURL()})`);
+            embed.setImage(user.user.displayAvatarURL({ dynamic: true }));
+
+            message.channel.send(`> Viewing avatar of ${user.user.username}`);
+            message.channel.send(embed);
+        } else {
+            embed.setDescription(`[**Image Link**](${message.author.avatarURL()})`);
+            embed.setImage(message.author.avatarURL({ dynamic: true }));
+
+            message.channel.send(`> Viewing avatar of ${message.author.username}`);
+            message.channel.send(embed)
+        }
+
     }
 
 }
