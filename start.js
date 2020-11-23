6c48e3c4932cf0781321a6c52e9126e0694f4961
TheRealKizu <therealkizu@protonmail.com>
2020-11-23 14:10:42 +0800

    chore: added .env checks

--- start.js
+++ start.js
@@ -1,4 +1,21 @@
-start();
+require("dotenv").config();
+checkEnv();
+
+function checkEnv() {
+    if (process.env.DISCORD_TOKEN === "") {
+        return console.error("Discord Bot Token shouldn't be null!");
+    }
+
+    if (process.env.OWNER_ID === "") {
+        return console.error("Owner User ID shouldn't be null!");
+    }
+
+    if (process.env.PREFIX === "") {
+        return console.error("Bot Prefix shouldn't be null!");
+    }
+
+    start();
+}
 
 function start() {
     // TODO: Check if host is Glitch or Heroku
