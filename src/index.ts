ddb6826db67617e61e2374260990007c912a1594
TheRealKizu <therealkizu@protonmail.com>
2020-12-02 16:56:05 +0800

    fix: readded owner ID

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { BaseClient } from "./structures/bot/BaseClient";
 import "dotenv/config";
 
 const client = new BaseClient({
+    ownerID: process.env.OWNER_ID,
     token: process.env.DISCORD_TOKEN
 }, {
     disableMentions: "everyone"
