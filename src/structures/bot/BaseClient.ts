c5dc05037add697971a4d6098daaf42f2e3ec10b
TheRealKizu <therealkizu@protonmail.com>
2020-12-03 12:33:20 +0800

    chore: make vars accessible in eval cmd and better directory checks

--- src/structures/bot/BaseClient.ts
+++ src/structures/bot/BaseClient.ts
@@ -1,18 +1,19 @@
 import { AkairoClient, CommandHandler, ListenerHandler } from "discord-akairo";
 import { ClientOptions } from "discord.js";
 import { join } from "path";
+import { existsSync } from "fs";
 import "dotenv/config";
 import { BaseClientOptions } from "../../../typings";
 
 export class BaseClient extends AkairoClient {
 
-    protected commandHandler: CommandHandler = new CommandHandler(this, {
+    public commandHandler: CommandHandler = new CommandHandler(this, {
         directory: join(__dirname, "../../", "commands"),
 
         prefix: process.env.PREFIX ?? "e!"
     });
 
-    protected listenerHandler: ListenerHandler = new ListenerHandler(this, {
+    public listenerHandler: ListenerHandler = new ListenerHandler(this, {
         directory: join(__dirname, "../../", "listeners")
     });
 
@@ -21,14 +22,18 @@ export class BaseClient extends AkairoClient {
     }
 
     async build() {
-        this.commandHandler.loadAll();
-        this.commandHandler.useListenerHandler(this.listenerHandler);
+        if (existsSync(this.commandHandler.directory)) {
+            this.commandHandler.loadAll();
+            this.commandHandler.useListenerHandler(this.listenerHandler);
+        }
 
-        this.listenerHandler.loadAll();
-        this.listenerHandler.setEmitters({
-            commandHandler: this.commandHandler,
-            listenerHandler: this.listenerHandler
-        });
+        if (existsSync(this.listenerHandler.directory)) {
+            this.listenerHandler.loadAll();
+            this.listenerHandler.setEmitters({
+                commandHandler: this.commandHandler,
+                listenerHandler: this.listenerHandler
+            });
+        }
 
         return this.login(process.env.DISCORD_TOKEN);
     }
