3c6ff2c40be0f5e5a573a4a136de603075e60df3
TheRealKizu <therealkizu@protonmail.com>
2021-01-10 09:25:07 +0800

    fix: fix vsc lints

--- src/listeners/ReadyListener.ts
+++ src/listeners/ReadyListener.ts
@@ -9,7 +9,7 @@ class ReadyListener extends BaseListener {
         });
     }
 
-    async exec(...args): Promise<any> {
+    async exec(...args: any[]): Promise<any> {
         await this.client.user.setPresence({
             activity: {
                 name: "home.elysianetwork.xyz — 🔵",
